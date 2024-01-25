f46cd6cfa08a66f17124fc77937a599d751d0760
melvinjariwala <melvinjariwala7.MJ@gmail.com>
2024-01-25 20:30:18 +0530

    Implemented Export Image feature

--- lib/screens/editing_screen.dart
+++ lib/screens/editing_screen.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
 import 'package:canvas_craft/models/text_data.dart';
 import 'package:canvas_craft/widgets/text_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'dart:typed_data';
+import 'package:flutter/rendering.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'dart:ui' as ui;
 
 class EditingScreen extends StatefulWidget {
   const EditingScreen({super.key});
@@ -12,10 +19,59 @@ class EditingScreen extends StatefulWidget {
 }
 
 class _EditingScreenState extends State<EditingScreen> {
+  final GlobalKey _globalKey = GlobalKey();
   List<TextData> textDataList = [];
   int selectedTextIndex = -1;
   List<List<TextData>> undoStack = [];
   List<List<TextData>> redoStack = [];
+
+  Future<Uint8List?> _capturePng() async {
+    try {
+      print("_globalKey.currentContext : ${_globalKey.currentContext}");
+      RenderRepaintBoundary boundary = _globalKey.currentContext!
+          .findRenderObject() as RenderRepaintBoundary;
+      print("Before toImage");
+
+      if (boundary.debugNeedsPaint) {
+        print("Waiting for boundary to be painted.");
+        await Future.delayed(const Duration(milliseconds: 20));
+        return _capturePng();
+      }
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      print("After toImage");
+      ByteData? byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List pngBytes = byteData!.buffer.asUint8List();
+      return pngBytes;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<void> _exportImage() async {
+    Uint8List? pngBytes = await _capturePng();
+    if (pngBytes != null) {
+      final result =
+          await ImageGallerySaver.saveImage(Uint8List.fromList(pngBytes));
+      if (result != null) {
+        Fluttertoast.showToast(
+            msg: "Image Saved Successfully!",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.green,
+            textColor: Colors.white);
+      } else {
+        Fluttertoast.showToast(
+            msg: "Failed to show image!",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.red,
+            textColor: Colors.white);
+      }
+      print(result);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +79,20 @@ class _EditingScreenState extends State<EditingScreen> {
       appBar: AppBar(
         title: const Text("Canvas Craft"),
         centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                _exportImage();
+              },
+              tooltip: "Export Image",
+              icon: const Icon(Icons.save))
+        ],
       ),
-      body: Stack(
-        children: [
-          GestureDetector(
-            onPanUpdate: (details) {
-              if (selectedTextIndex != -1) {
-                setState(() {
-                  textDataList[selectedTextIndex].position += details.delta;
-                  _addUndoSnapshot();
-                });
-              }
-            },
-            child: PhotoViewGallery.builder(
+      body: RepaintBoundary(
+        key: _globalKey,
+        child: Stack(
+          children: [
+            PhotoViewGallery.builder(
               itemCount: 1,
               builder: (context, index) {
                 return PhotoViewGalleryPageOptions(
@@ -46,48 +103,58 @@ class _EditingScreenState extends State<EditingScreen> {
               scrollPhysics: const BouncingScrollPhysics(),
               backgroundDecoration: const BoxDecoration(color: Colors.black),
             ),
-          ),
-          ...textDataList.asMap().entries.map((entry) {
-            int index = entry.key;
-            TextData textData = entry.value;
-            return Positioned(
-                left: textData.position.dx,
-                top: textData.position.dy,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedTextIndex = index;
-                    });
-                  },
-                  onDoubleTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => TextDialog(textData: textData),
-                    ).then((value) {
-                      if (value != null) {
+            ...textDataList.asMap().entries.map((entry) {
+              int index = entry.key;
+              TextData textData = entry.value;
+              return Positioned(
+                  left: textData.position.dx,
+                  top: textData.position.dy,
+                  child: GestureDetector(
+                    onPanUpdate: (details) {
+                      if (selectedTextIndex != -1) {
                         setState(() {
-                          textDataList[index] = value;
+                          textDataList[selectedTextIndex].position +=
+                              details.delta;
                           _addUndoSnapshot();
                         });
                       }
-                    });
-                  },
-                  child: SizedBox(
-                    width: textData.boxSize,
-                    height: textData.boxSize,
-                    child: Text(
-                      textData.text,
-                      style: TextStyle(
-                          color: textData.textColor,
-                          fontSize: textData.fontSize,
-                          fontFamily: textData.fontStyle != "Default"
-                              ? textData.fontStyle
-                              : null),
+                    },
+                    onTap: () {
+                      setState(() {
+                        selectedTextIndex = index;
+                      });
+                    },
+                    onDoubleTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) =>
+                            TextDialog(textData: textData),
+                      ).then((value) {
+                        if (value != null) {
+                          setState(() {
+                            textDataList[index] = value;
+                            _addUndoSnapshot();
+                          });
+                        }
+                      });
+                    },
+                    child: SizedBox(
+                      width: textData.boxSize,
+                      height: textData.boxSize,
+                      child: Text(
+                        textData.text,
+                        style: TextStyle(
+                            color: textData.textColor,
+                            fontSize: textData.fontSize,
+                            fontFamily: textData.fontStyle != "Default"
+                                ? textData.fontStyle
+                                : null),
+                      ),
                     ),
-                  ),
-                ));
-          }).toList()
-        ],
+                  ));
+            }).toList()
+          ],
+        ),
       ),
       floatingActionButton: Stack(
         children: [
@@ -117,7 +184,8 @@ class _EditingScreenState extends State<EditingScreen> {
                   onPressed: () {
                     showDialog(
                         context: context,
-                        builder: (context) => const TextDialog()).then((value) {
+                        builder: (BuildContext context) =>
+                            const TextDialog()).then((value) {
                       if (value != null) {
                         setState(() {
                           textDataList.add(value);
